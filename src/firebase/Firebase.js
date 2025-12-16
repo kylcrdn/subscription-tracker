eb08abe4a313ccd2b0ac7faabef6d87d7f452895
kyle Cardano <aquaaqua7080@gmail.com>
2025-12-16 23:05:12 +0100

    fixed firebase

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -1,20 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBDcFWW1xwOc9_504QBKRX9idbMP9tr1qw",
-  authDomain: "subscription-tracker-400c0.firebaseapp.com",
-  projectId: "subscription-tracker-400c0",
-  storageBucket: "subscription-tracker-400c0.firebasestorage.app",
-  messagingSenderId: "821038498308",
-  appId: "1:821038498308:web:cb55075a68f9e81ac1eb51",
-  measurementId: "G-D7LVWZ2GE7",
+  apiKey: "your_apiKey",
+  authDomain: "your_authDomain",
+  projectId: "your_projectId",
+  storageBucket: "your_storageBucket",
+  messagingSenderId: "your_messagingSenderId",
+  appId: "your_appId",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const googleProvider = new GoogleAuthProvider();
 
-export { auth, googleProvider };
+export { app, auth };
