d3516cea5a0bd31b51a3054359f43a6c104d0d87
kyle Cardano <aquaaqua7080@gmail.com>
2026-01-20 17:05:55 +0100

    null

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -2,20 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-/**
- * 
-const firebaseConfig = {
-  apiKey: "AIzaSyBDcFWW1xwOc9_504QBKRX9idbMP9tr1qw",
-  authDomain: "subscription-tracker-400c0.firebaseapp.com",
-  projectId: "subscription-tracker-400c0",
-  storageBucket: "subscription-tracker-400c0.firebasestorage.app",
-  messagingSenderId: "821038498308",
-  appId: "1:821038498308:web:cb55075a68f9e81ac1eb51",
-  measurementId: "G-D7LVWZ2GE7",
-};
- * 
- * */
-
 const firebaseConfig = {
   apiKey: "AIzaSyBDcFWW1xwOc9_504QBKRX9idbMP9tr1qw",
   authDomain: "subscription-tracker-400c0.firebaseapp.com",
